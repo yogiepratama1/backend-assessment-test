c5545e24553b9ae122fbad732fe5268be6d14c5e
unknown <pratamayogie131@gmail.com>
2023-03-21 02:42:46 +0700

    added create loan function

--- app/Services/LoanService.php
+++ app/Services/LoanService.php
@@ -21,7 +21,36 @@ class LoanService
      */
     public function createLoan(User $user, int $amount, string $currencyCode, int $terms, string $processedAt): Loan
     {
-        //
+        $loan = Loan::create([
+            'user_id' => $user->id,
+            'amount' => $amount,
+            'terms' => $terms,
+            'outstanding_amount' => $amount,
+            'currency_code' => $currencyCode,
+            'processed_at' => $processedAt,
+            'status' => Loan::STATUS_DUE,
+        ]);
+
+        $data = [];
+
+        for ($i = 1; $i <= $terms; $i++) {
+            $dueDate = date('Y-m-d', strtotime($processedAt . ' + ' . $i . ' month'));
+            $totalAmount = floor($amount / $terms);
+            if ($i == $terms) {
+                $totalAmount = floor($amount - ($totalAmount * ($terms - 1)));
+            }
+
+            $data[] = [
+                'amount' => $totalAmount,
+                'outstanding_amount' => $totalAmount,
+                'currency_code' => $currencyCode,
+                'due_date' => $dueDate
+            ];
+        }
+
+        $loan->scheduledRepayments()->createMany($data);
+
+        return $loan;
     }
 
     /**
