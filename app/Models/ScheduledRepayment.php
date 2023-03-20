9c60491336b78c90492f8c07b5a6bed68093da11
unknown <pratamayogie131@gmail.com>
2023-03-21 02:41:26 +0700

    fixed typo

--- app/Models/ScheduledRepayment.php
+++ app/Models/ScheduledRepayment.php
@@ -6,16 +6,20 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class ReceivedRepayment extends Model
+class ScheduledRepayment extends Model
 {
     use HasFactory;
+
+    public const STATUS_DUE = 'due';
+    public const STATUS_PARTIAL = 'partial';
+    public const STATUS_REPAID = 'repaid';
 
     /**
      * The table associated with the model.
      *
      * @var string
      */
-    protected $table = 'received_repayments';
+    protected $table = 'scheduled_repayments';
 
     /**
      * The attributes that are mass assignable.
@@ -27,7 +31,7 @@ class ReceivedRepayment extends Model
     ];
 
     /**
-     * A Received Repayment belongs to a Loan
+     * A Scheduled Repayment belongs to a Loan
      *
      * @return BelongsTo
      */
