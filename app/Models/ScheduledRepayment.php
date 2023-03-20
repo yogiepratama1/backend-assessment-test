723f89d23913ab60bd5962a811acd2309db6f34d
unknown <pratamayogie131@gmail.com>
2023-03-21 02:40:37 +0700

    added fillable attributes in scheduled repayment class

--- app/Models/ScheduledRepayment.php
+++ app/Models/ScheduledRepayment.php
@@ -6,32 +6,28 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class ScheduledRepayment extends Model
+class ReceivedRepayment extends Model
 {
     use HasFactory;
-
-    public const STATUS_DUE = 'due';
-    public const STATUS_PARTIAL = 'partial';
-    public const STATUS_REPAID = 'repaid';
 
     /**
      * The table associated with the model.
      *
      * @var string
      */
-    protected $table = 'scheduled_repayments';
+    protected $table = 'received_repayments';
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $fillable = [
-        //
+    protected $guarded = [
+        'id'
     ];
 
     /**
-     * A Scheduled Repayment belongs to a Loan
+     * A Received Repayment belongs to a Loan
      *
      * @return BelongsTo
      */
